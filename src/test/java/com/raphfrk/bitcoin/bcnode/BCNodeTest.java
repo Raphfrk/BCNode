148e95c7c66df49cd70c2acb179d7cf4b068441e
Raphfrk <raphfrk@gmail.com>
2013-02-10 11:54:39 +0000

    Set project license - Adds LICENSE file - Maven set to automatically add headers

--- src/test/java/com/raphfrk/bitcoin/bcnode/BCNodeTest.java
+++ src/test/java/com/raphfrk/bitcoin/bcnode/BCNodeTest.java
@@ -1,3 +1,26 @@
+/*
+ * This file is part of BCNode.
+ *
+ * Copyright (c) Raphfrk 2013 <www.raphfrk.com/bcnode>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+ * of the Software, and to permit persons to whom the Software is furnished to do
+ * so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
 package com.raphfrk.bitcoin.bcnode;
 
 import junit.framework.Test;
@@ -7,32 +30,27 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class BCNodeTest
-    extends TestCase
-{
+public class BCNodeTest extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public BCNodeTest( String testName )
-    {
+    public BCNodeTest( String testName ) {
         super( testName );
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
+    public static Test suite() {
         return new TestSuite( BCNodeTest.class );
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testBCNode()
-    {
+    public void testBCNode() {
         assertTrue( true );
     }
 }
